021b5caf7cfa2efb24e0516b924cfd698b272b6c
paulblanche21 <paulblanche21@gmail.com>
2023-12-03 22:48:37 +0000

    minor changes

--- arena_app/admin.py
+++ arena_app/admin.py
@@ -7,33 +7,40 @@ from .models import (
 
 # USERS
 
+
 @admin.register(SubscriptionPlan)
 class SubscriptionPlanAdmin(admin.ModelAdmin):
     list_display = ('name', 'price_monthly', 'price_yearly')
+
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ('name', 'username', 'email', 'subscription_status', 'subscription_type', 'subscription_plan')
     search_fields = ('user__username', 'user__email', 'name')
 
+
 @admin.register(Tip)
 class TipAdmin(admin.ModelAdmin):
     list_display = ('user', 'content', 'created_at')
     search_fields = ('user__username', 'content')
 
+
 @admin.register(Follower)
 class FollowerAdmin(admin.ModelAdmin):
     list_display = ('user', 'follower')
 
+
 @admin.register(ChatMessage)
 class ChatMessageAdmin(admin.ModelAdmin):
     list_display = ('user', 'content', 'created_at')
+
 
 @admin.register(Subscription)
 class SubscriptionAdmin(admin.ModelAdmin):
     list_display = ('user', 'start_date', 'end_date', 'status', 'subscription_type')
 
 # SPORTS
+
 
 @admin.register(Sport)
 class SportAdmin(admin.ModelAdmin):
@@ -49,9 +56,11 @@ class TeamAdmin(admin.ModelAdmin):
 class FixtureAdmin(admin.ModelAdmin):
     list_display = ('sport', 'team_home', 'team_away', 'date_time')
 
+
 @admin.register(Result)
 class ResultAdmin(admin.ModelAdmin):
     list_display = ('fixture', 'team_home_score', 'team_away_score')
+
 
 @admin.register(LiveScore)
 class LiveScoreAdmin(admin.ModelAdmin):
