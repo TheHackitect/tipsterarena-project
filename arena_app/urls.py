8a766d5a8b18731d9e215f407d9f3e1dd4f5c80b
paulblanche21 <paulblanche21@gmail.com>
2023-12-05 00:06:41 +0000

    minor changes

--- arena_app/urls.py
+++ arena_app/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path, re_path
 from django.contrib import admin
-
-from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import handler404
+from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
